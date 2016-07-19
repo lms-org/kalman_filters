58b21539156681d93d9a5620e0968651879d7b43
phibedy <philipp.schmette@gmx.de>
2016-07-19 06:57:08 +0200

    fixed diff sign, added simple qt chart, WIP

--- include/utils.h
+++ include/utils.h
@@ -9,13 +9,15 @@ namespace KALMAN_FILTER{
     const double DOUBLE_H = 1e-7;
     template<typename M,typename T>
     M differentiate(std::function<T(M)> f,const M& x, T h){
-        M res;
-        M vh;
-        vh.setZero(h);
+        //Needed if M contains Eigen::Dynamic
+        M res = x;
+        M vh = x;
+        vh.setZero();
+        res.setZero();
         for(int r = 0; r < x.rows(); r++){
             for(int c = 0; c < x.cols(); c++){
                 vh(r,c) = h;
-                res(r,c)= (f(x-vh)-f(x+vh))/(2*h);
+                res(r,c)= (f(x+vh)-f(x-vh))/(2*h);
                 vh(r,c) = 0;
             }
         }
@@ -23,7 +25,7 @@ namespace KALMAN_FILTER{
     }
 
     double differentiate(std::function<double(double)> f,float x, double h = 1e-7){
-        return (f(x-h)-f(x+h))/(2*h);
+        return (f(x+h)-f(x-h))/(2*h);
     }
 }
 
--- src/foo.cpp
+++ src/foo.cpp
@@ -1,2 +1,86 @@
 #include <kalman_filter/mass_ekf.h>
 #include <filter/line_line_x.h>
+#include <iostream>
+#include <Eigen/Eigen>
+#include <cmath>
+
+
+#include <QtGui/QGuiApplication>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtCharts/QChartView>
+#include <QtCharts/QSplineSeries>
+#include <QtCharts/QScatterSeries>
+
+int main(int argc, char** argv){
+    (void)argc;
+    (void)argv;
+    std::function<double(double)> f1 = [](double x){
+        return x*x;
+    };
+
+    std::cout<<(KALMAN_FILTER::differentiate(f1,3))<<std::endl;
+    std::cout<<"###"<<std::endl;
+
+    std::function<double(Eigen::VectorXd)> f2 =[](Eigen::VectorXd x){
+        return cos(x(0))+sin(x(1))+x(2)*x(2);
+    };
+    Eigen::VectorXd pos(3);
+    pos(0)=M_PI/2.0;
+    pos(1)=M_PI/2.0;
+    pos(2)=2;
+    std::cout<<(KALMAN_FILTER::differentiate<Eigen::VectorXd,double>(f2,pos,KALMAN_FILTER::DOUBLE_H))<<std::endl;
+
+
+    //QWT http://qwt.sourceforge.net/
+    //oder einfach nur qt5 http://doc.qt.io/qt-5/qtdatavisualization-index.html
+
+    QT_CHARTS_USE_NAMESPACE
+
+    QApplication app(argc, argv);
+
+    QLineSeries fit;
+    QScatterSeries points;
+
+    QChart *chart = new QChart();
+    chart->legend()->hide();
+    chart->addSeries(&fit);
+    chart->addSeries(&points);
+    chart->createDefaultAxes();
+    chart->setTitle("Simple line chart example");
+
+    QChartView chartView(chart);
+    chartView.setRenderHint(QPainter::Antialiasing);
+
+    QMainWindow window;
+    window.setCentralWidget(&chartView);
+    window.resize(400, 300);
+    window.show();
+    chart->axisX()->setRange(0,10);
+    chart->axisY()->setRange(0,10);
+    while(true){
+        //create new measurement-points
+        Eigen::Matrix<double,10,2> mData;
+        double d = 0.2;
+        double offset = 0;
+        float randomF = 1;
+        for(int i = 0; i < 10; i++){
+            float r = ((double) rand() / (RAND_MAX));
+            mData(i,0) = i;
+            mData(i,1) = d*i+offset + randomF*r;
+        }
+
+        //conversion to qt points
+        points.clear();
+        for(int i = 0; i < 10; i++){
+            points.append(mData(i,0),mData(i,1));
+        }
+
+        //update the gui
+        app.processEvents();
+
+        //timer
+        usleep(200);
+    }
+
+}
